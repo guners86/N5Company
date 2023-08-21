a4a3402a1bf0db16e1fd6e00b1d9d7817b4b25be
Damian Arturo Garces Paez <guners86@hotmail.com>
2023-08-21 15:44:52 -0500

    Add generic validations

---
+++ N5Company.Core.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N5Company.Core.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> Errors { get; }
+
+        public ValidationException() : base("One or more validations errors")
+        {
+            Errors = new List<string>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures) 
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
