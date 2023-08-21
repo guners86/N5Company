114206ed7da327cb19b8427cdd1a6427ed0ad7b5
Damian Arturo Garces Paez <guners86@hotmail.com>
2023-08-21 15:22:51 -0500

    Add services in core aplication and reference in WebAPi aplication

---
+++ N5Company.Core.Application/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace N5Company.Core.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
